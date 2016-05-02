1ba32cc635c820e6cbafd903f412c873529c62a9
khalilH <halifa.ladislas@gmail.com>
2016-05-02 14:09:58 +0200

    update Test Level 3

--- src/tests/TestLevelAbstract.java
+++ src/tests/TestLevelAbstract.java
@@ -48,7 +48,7 @@ public abstract class TestLevelAbstract extends AssertionTests{
 	 *  init(25,20)
 	 *  
 	 *  Oracle:
-	 *  Pas d'exception &&
+	 *  Pas de ContractError &&
 	 *  getWidth() == 20 &&
 	 *  getHeight() == 25 &&
 	 *  isEditing() == true &&
@@ -78,7 +78,7 @@ public abstract class TestLevelAbstract extends AssertionTests{
 	/**
 	 * Objectif 1: init
 	 * 
-	 * Cas 1_1: init: error width <=0
+	 * Cas 1_1: init: error w <=0
 	 * 
 	 * Condition initial:
 	 * aucune
@@ -87,7 +87,7 @@ public abstract class TestLevelAbstract extends AssertionTests{
 	 *  init(0,25)
 	 *  
 	 *  Oracle:
-	 *  Exception pour init
+	 *  ContractError pour init
 	 */
 	@Test
 	public void testInit1_1() {
@@ -105,7 +105,7 @@ public abstract class TestLevelAbstract extends AssertionTests{
 	/**
 	 * Objectif 1: init
 	 * 
-	 * Cas 1_2: init: error height <=0
+	 * Cas 1_2: init: error h <=0
 	 * 
 	 * Condition initial:
 	 * aucune
@@ -114,7 +114,7 @@ public abstract class TestLevelAbstract extends AssertionTests{
 	 *  init(25,0)
 	 *  
 	 *  Oracle:
-	 *  Exception pour init
+	 *  ContractError pour init
 	 */
 	@Test
 	public void testInit1_2() {
@@ -143,7 +143,7 @@ public abstract class TestLevelAbstract extends AssertionTests{
 	 * getNature(3,3)
 	 *  
 	 *  Oracle:
-	 *  Pas d'exception
+	 *  Pas de ContractError
 	 */
 	@Test
 	public void testGetNature2_0() {
@@ -171,7 +171,7 @@ public abstract class TestLevelAbstract extends AssertionTests{
 	/**
 	 * Objectif 2: getNature
 	 * 
-	 * Cas 2_1: getNature error width < 0
+	 * Cas 2_1: getNature error x < 0
 	 * 
 	 * Condition initial:
 	 * init(10,10)
@@ -180,7 +180,7 @@ public abstract class TestLevelAbstract extends AssertionTests{
 	 * getNature(-2,5)
 	 *  
 	 *  Oracle:
-	 *  Exception pour getNature
+	 *  ContractError pour getNature
 	 */
 	@Test
 	public void testGetNature2_1() {
@@ -205,7 +205,7 @@ public abstract class TestLevelAbstract extends AssertionTests{
 	/**
 	 * Objectif 2: getNature
 	 * 
-	 * Cas 2_2: getNature error width >= getWidth()
+	 * Cas 2_2: getNature error x >= getWidth()
 	 * 
 	 * Condition initial:
 	 * init(10,10)
@@ -214,7 +214,7 @@ public abstract class TestLevelAbstract extends AssertionTests{
 	 * getNature(15,5)
 	 *  
 	 *  Oracle:
-	 *  Exception pour getNature
+	 *  ContractError pour getNature
 	 */
 	@Test
 	public void testGetNature2_2() {
@@ -240,7 +240,7 @@ public abstract class TestLevelAbstract extends AssertionTests{
 	/**
 	 * Objectif 2: getNature
 	 * 
-	 * Cas 2_3: getNature error height < 0 
+	 * Cas 2_3: getNature error y < 0 
 	 * 
 	 * Condition initial:
 	 * init(10,10)
@@ -249,7 +249,7 @@ public abstract class TestLevelAbstract extends AssertionTests{
 	 * getNature(3,-2)
 	 *  
 	 *  Oracle:
-	 *  Exception pour getNature
+	 *  ContractError pour getNature
 	 */
 	@Test
 	public void testGetNature2_3() {
@@ -274,7 +274,7 @@ public abstract class TestLevelAbstract extends AssertionTests{
 	/**
 	 * Objectif 2: getNature
 	 * 
-	 * Cas 2_4: getNature error height >= getHeight() 
+	 * Cas 2_4: getNature error y >= getHeight() 
 	 * 
 	 * Condition initial:
 	 * init(10,10)
@@ -283,7 +283,7 @@ public abstract class TestLevelAbstract extends AssertionTests{
 	 * getNature(3,12)
 	 *  
 	 *  Oracle:
-	 *  Exception pour getNature
+	 *  ContractError pour getNature
 	 */
 	@Test
 	public void testGetNature2_4() {
@@ -319,7 +319,7 @@ public abstract class TestLevelAbstract extends AssertionTests{
 	 * isExit(3,3)
 	 *  
 	 *  Oracle:
-	 *  Pas d'exception
+	 *  Pas de ContractError
 	 */
 	@Test
 	public void testIsExit3_0() {
@@ -347,7 +347,7 @@ public abstract class TestLevelAbstract extends AssertionTests{
 	/**
 	 * Objectif 3: isExit
 	 * 
-	 * Cas 3_1: isExit error width < 0
+	 * Cas 3_1: isExit error x < 0
 	 * 
 	 * Condition initial:
 	 * init(10,10)
@@ -356,7 +356,7 @@ public abstract class TestLevelAbstract extends AssertionTests{
 	 * isExit(-2,5)
 	 *  
 	 *  Oracle:
-	 *  Exception pour isExit
+	 *  ContractError pour isExit
 	 */
 	@Test
 	public void testIsExit3_1() {
@@ -381,7 +381,7 @@ public abstract class TestLevelAbstract extends AssertionTests{
 	/**
 	 * Objectif 3: isExit
 	 * 
-	 * Cas 3_2: isExit error width >= getWidth()
+	 * Cas 3_2: isExit error x >= getWidth()
 	 * 
 	 * Condition initial:
 	 * init(10,10)
@@ -390,7 +390,7 @@ public abstract class TestLevelAbstract extends AssertionTests{
 	 * isExit(15,5)
 	 *  
 	 *  Oracle:
-	 *  Exception pour isExit
+	 *  ContractError pour isExit
 	 */
 	@Test
 	public void testIsExit3_2() {
@@ -416,7 +416,7 @@ public abstract class TestLevelAbstract extends AssertionTests{
 	/**
 	 * Objectif 3: isExit
 	 * 
-	 * Cas 3_3: isExit error height < 0 
+	 * Cas 3_3: isExit error y < 0 
 	 * 
 	 * Condition initial:
 	 * init(10,10)
@@ -425,7 +425,7 @@ public abstract class TestLevelAbstract extends AssertionTests{
 	 * isExit(3,-2)
 	 *  
 	 *  Oracle:
-	 *  Exception pour isExit
+	 *  ContractError pour isExit
 	 */
 	@Test
 	public void testIsExit3_3() {
@@ -450,7 +450,7 @@ public abstract class TestLevelAbstract extends AssertionTests{
 	/**
 	 * Objectif 3: isExit
 	 * 
-	 * Cas 3_4: isExit error height >= getHeight() 
+	 * Cas 3_4: isExit error y >= getHeight() 
 	 * 
 	 * Condition initial:
 	 * init(10,10)
@@ -459,7 +459,7 @@ public abstract class TestLevelAbstract extends AssertionTests{
 	 * isExit(3,12)
 	 *  
 	 *  Oracle:
-	 *  Exception pour isExit
+	 *  ContractError pour isExit
 	 */
 	@Test
 	public void testIsExit3_4() {
@@ -481,10 +481,12 @@ public abstract class TestLevelAbstract extends AssertionTests{
 		}
 	}
 
+	//TODO faire isEntrance
+
 	/**
-	 * Objectif 4: setNature
+	 * Objectif 6: setNature
 	 * 
-	 * Cas 4_0: setNature positif 
+	 * Cas 6_0: setNature positif 
 	 * 
 	 * Condition initial:
 	 * init(10,10)
@@ -493,11 +495,11 @@ public abstract class TestLevelAbstract extends AssertionTests{
 	 * setNature(5, 5, Nature.DIRT)
 	 *  
 	 *  Oracle:
-	 *  Pas d'exception && getNature(5, 5) == Nature.DIRT
+	 *  Pas de ContractError && getNature(5, 5) == Nature.DIRT
 	 */
 	@Test
-	public void testSetNature() {
-		String test = "Level Test Objectif 4.0";
+	public void testSetNature6_0() {
+		String test = "Level Test Objectif 6.0";
 		try {
 			// condition initiale
 			level.init(10, 10);
@@ -515,103 +517,384 @@ public abstract class TestLevelAbstract extends AssertionTests{
 		}
 	}
 
-	//TODO continuer ici
+	/**
+	 * Objectif 6: setNature
+	 * 
+	 * Cas 6_1: setNature negatif x < 0 
+	 * 
+	 * Condition initiale:
+	 * init(10,10)
+	 * 
+	 * Operation:
+	 * setNature(-3, 5, Nature.DIRT)
+	 *  
+	 *  Oracle:
+	 *  ContractError pour setNature
+	 */
 	@Test
-	public void testSetNatureFail1() {
+	public void testSetNature6_1() {
+		String test = "Level Test Objectif 6.1";
 		try {
+			// condition initiale
 			level.init(10, 10);
-			level.setNature(5, 15, Nature.DIRT);
-			assertTrue(false);
+			try {
+				//operation
+				level.setNature(-2, 5, Nature.DIRT);
+				// oracle
+				assertion(test+": setNature doit echouer", false);
+			} catch (ContractError e) {
+				assertion(test, true);
+			} 
 		} catch (ContractError e) {
-			assertTrue(true);
+			assertion(test+": erreur a l'initialisation du test: "+e.getMessage(), false);
 		}
 	}
 
+	/**
+	 * Objectif 6: setNature
+	 * 
+	 * Cas 6_2: setNature error x > getWidth() 
+	 * 
+	 * Condition initiale:
+	 * init(10,10)
+	 * 
+	 * Operation:
+	 * setNature(15, 5, Nature.DIRT)
+	 *  
+	 *  Oracle:
+	 *  ContractError pour setNature
+	 */
 	@Test
-	public void testSetNatureFail2() {
+	public void testSetNature6_2() {
+		String test = "Level Test Objectif 6.2";
 		try {
+			// condition initiale
 			level.init(10, 10);
-			level.setNature(5, -5, Nature.DIRT);
-			assertTrue(false);
+			try {
+				//operation
+				level.setNature(15, 5, Nature.DIRT);
+				// oracle
+				assertion(test+": setNature doit echouer", false);
+			} catch (ContractError e) {
+				assertion(test, true);
+			} 
 		} catch (ContractError e) {
-			assertTrue(true);
+			assertion(test+": erreur a l'initialisation du test: "+e.getMessage(), false);
 		}
 	}
 
+	/**
+	 * Objectif 6: setNature
+	 * 
+	 * Cas 6_3: setNature error y < 0 
+	 * 
+	 * Condition initiale:
+	 * init(10,10)
+	 * 
+	 * Operation:
+	 * setNature(5, -5, Nature.DIRT)
+	 *  
+	 *  Oracle:
+	 *  ContractError pour setNature
+	 */
 	@Test
-	public void testSetEntrance() {
+	public void testSetNature6_3() {
+		String test = "Level Test Objectif 6.3";
 		try {
+			// condition initiale
 			level.init(10, 10);
-			level.setEntrance(5, 5);
-			assertTrue(level.isEntrance(5, 5) == true);
-			assertTrue(true);
+			try {
+				//operation
+				level.setNature(5, -5, Nature.DIRT);
+				// oracle
+				assertion(test+": setNature doit echouer", false);
+			} catch (ContractError e) {
+				assertion(test, true);
+			} 
 		} catch (ContractError e) {
-			assertTrue(false);
+			assertion(test+": erreur a l'initialisation du test: "+e.getMessage(), false);
 		}
 	}
 
+	/**
+	 * Objectif 6: setNature
+	 * 
+	 * Cas 6_4: setNature error y > getHeight() 
+	 * 
+	 * Condition initiale:
+	 * init(10,10)
+	 * 
+	 * Operation:
+	 * setNature(5, 15, Nature.DIRT)
+	 *  
+	 *  Oracle:
+	 *  ContractError pour setNature
+	 */
 	@Test
-	public void testSetEntranceFail1() {
+	public void testSetNature6_4() {
+		String test = "Level Test Objectif 6.4";
 		try {
+			// condition initiale
+			level.init(10, 10);
+			try {
+				//operation
+				level.setNature(5, 15, Nature.DIRT);
+				// oracle
+				assertion(test+": setNature doit echouer", false);
+			} catch (ContractError e) {
+				assertion(test, true);
+			} 
+		} catch (ContractError e) {
+			assertion(test+": erreur a l'initialisation du test: "+e.getMessage(), false);
+		}
+	}
+
+	/**
+	 * Objectif 7: setEntrance
+	 * 
+	 * Cas 7_0: setEntrance positif 
+	 * 
+	 * Condition initiale:
+	 * init(10,10)
+	 * 
+	 * Operation:
+	 * setEntrance(5, 5)
+	 *  
+	 *  Oracle:
+	 *  Pas de ContractError && isEntrance(5, 5) == true
+	 */
+	@Test
+	public void testSetEntrance7_0() {
+		String test = "Level Test Objectif 7.0";
+		try {
+			//Condition initiale 
+			level.init(10, 10);
+			try {
+				// Operation
+				level.setEntrance(5, 5);
+
+				//oracle
+				assertion(test, level.isEntrance(5, 5) == true);
+				assertion(test, true);
+			} catch (ContractError e) {
+				assertion(test+": "+e.getMessage(), false);
+			}
+		} catch (ContractError e) {
+			assertion(test+": erreur a l'initialisation du test: "+e.getMessage(), false);
+		}
+	}
+
+	/**
+	 * Objectif 7: setEntrance
+	 * 
+	 * Cas 7_1: setEntrance error la case au dessus n'est pas vide 
+	 * 
+	 * Condition initiale:
+	 * init(10,10)
+	 * setNature(5,4,Nature.DIRT)
+	 * 
+	 * Operation:
+	 * setEntrance(5, 5)
+	 *  
+	 *  Oracle:
+	 *  ContractError pour setEntrance
+	 */
+	@Test
+	public void testSetEntrance7_1() {
+		String test = "Level Test Objectif 7.1";
+		try {
+			//Condition initiale
 			level.init(10, 10);
 			level.setNature(5, 4, Nature.DIRT);
-			level.setEntrance(5, 5);
-			assertTrue(false);
+			try {
+				// Operation
+				level.setEntrance(5, 5);
+
+				//oracle
+				assertion(test+": setEntrance doit echouer", false);
+			} catch (ContractError e) {
+				assertion(test, true);
+			}
 		} catch (ContractError e) {
-			assertTrue(true);
+			assertion(test+": erreur a l'initialisation du test: "+e.getMessage(), false);
 		}
 	}
 
+
+
+	/**
+	 * Objectif 7: setEntrance
+	 * 
+	 * Cas 7_2: setEntrance errorla case en dessous n'est pas vide 
+	 * 
+	 * Condition initiale:
+	 * init(10,10)
+	 * setNature(5,6,Nature.METAL)
+	 * 
+	 * Operation:
+	 * setEntrance(5, 5)
+	 *  
+	 *  Oracle:
+	 *  ContractError pour setEntrance
+	 */
 	@Test
-	public void testSetEntranceFail2() {
+	public void testSetEntrance7_2() {
+		String test = "Level Test Objectif 7.2";
 		try {
+			//Condition initiale
 			level.init(10, 10);
 			level.setNature(5, 6, Nature.METAL);
-			level.setEntrance(5, 5);
-			assertTrue(false);
+			try {
+				// Operation
+				level.setEntrance(5, 5);
+				//oracle
+				assertion(test+": setEntrance doit echouer", false);
+			} catch (ContractError e) {
+				assertion(test, true);
+			}
 		} catch (ContractError e) {
-			assertTrue(true);
+			assertion(test+": erreur a l'initialisation du test: "+e.getMessage(), false);
 		}
 	}
 
+	/**
+	 * Objectif 8: setExit
+	 * 
+	 * Cas 8_0: setExit positif 
+	 * 
+	 * Condition initiale:
+	 * init(10,10)
+	 * setNature(5, 6, Nature.METAL)
+	 * 
+	 * Operation:
+	 * setExit(5, 5)
+	 *  
+	 *  Oracle:
+	 *  Pas de ContractError && isExit(5, 5) == true
+	 */
 	@Test
-	public void testSetExit() {
+	public void testSetExit8_0() {
+		String test = "Level Test Objectif 8.0";
 		try {
+			//Condition initiale 
 			level.init(10, 10);
-			level.setNature(5, 6, Nature.METAL);
-			level.setExit(5, 5);
-			assertTrue(level.isExit(5, 5) == true);
-			assertTrue(true);
+			level.setNature(5, 5, Nature.METAL);
+			try {
+				// Operation
+				level.setExit(5, 5);
+
+				//oracle
+				assertion(test, level.isExit(5, 5) == true);
+				assertion(test, true);
+			} catch (ContractError e) {
+				assertion(test+": "+e.getMessage(), false);
+			}
 		} catch (ContractError e) {
-			assertTrue(false);
+			assertion(test+": erreur a l'initialisation du test: "+e.getMessage(), false);
 		}
 	}
 
+	/**
+	 * Objectif 8: setExit
+	 * 
+	 * Cas 8_1: setExit error la case en dessous n'est pas METAL 
+	 * 
+	 * Condition initiale:
+	 * init(10,10)
+	 * setNature(5,6,Nature.DIRT)
+	 * 
+	 * Operation:
+	 * setExit(5, 5)
+	 *  
+	 *  Oracle:
+	 *  ContractError pour setExit
+	 */
 	@Test
-	public void testSetExitFail1() {
+	public void testSetExit8_1() {
+		String test = "Level Test Objectif 8.1";
 		try {
+			//Condition initiale
 			level.init(10, 10);
-			level.setNature(5, 6, Nature.DIRT);
-			level.setExit(5, 5);
-			assertTrue(false);
+			level.setNature(5,6,Nature.DIRT);
+			try {
+				// Operation
+				level.setExit(5, 5);
+				//oracle
+				assertion(test+": setExit doit echouer", false);
+			} catch (ContractError e) {
+				assertion(test, true);
+			}
 		} catch (ContractError e) {
-			assertTrue(true);
+			assertion(test+": erreur a l'initialisation du test: "+e.getMessage(), false);
 		}
 	}
 
+
+
+	/**
+	 * Objectif 8: setExit
+	 * 
+	 * Cas 8_2: setExit error la case au dessus n'est pas vide 
+	 * 
+	 * Condition initiale:
+	 * init(10,10)
+	 * setNature(5,4,Nature.METAL)
+	 * 
+	 * Operation:
+	 * setExit(5, 5)
+	 *  
+	 *  Oracle:
+	 *  ContractError pour setExit
+	 */
 	@Test
-	public void testSetExitFail2() {
+	public void testSetExit8_2() {
+		String test = "Level Test Objectif 8.2";
 		try {
+			//Condition initiale
 			level.init(10, 10);
-			level.setExit(5, 5);
-			assertTrue(false);
+			level.setNature(5, 4, Nature.METAL);
+			try {
+				// Operation
+				level.setExit(5, 5);
+				//oracle
+				assertion(test+": setExit doit echouer", false);
+			} catch (ContractError e) {
+				assertion(test, true);
+			}
 		} catch (ContractError e) {
-			assertTrue(true);
+			assertion(test+": erreur a l'initialisation du test: "+e.getMessage(), false);
 		}
 	}
 
+
+	/**
+	 * Objectif 9: goPlay
+	 * 
+	 * Cas 9_0: goPlay positif 
+	 * 
+	 * Condition initiale:
+	 * init(10,10)
+	 * for (int x = 0; x<level.getWidth(); x++) {
+	 * 		for (int y = 0; y<level.getHeight(); y++) {
+	 *			if (x == 0 || x == level.getWidth()-1 
+	 *						|| y == 0 || y == level.getHeight()-1) {
+	 *					level.setNature(x, y, Nature.METAL);
+	 *			}
+	 *		}
+	 *	}
+	 *	level.setEntrance(8, 5);
+	 *	level.setNature(5, 6, Nature.METAL);
+	 *	level.setExit(5, 5);
+	 * 
+	 * Operation:
+	 * goPlay()
+	 *  
+	 *  Oracle:
+	 *  Pas de ContractError && isEditing() == false
+	 */
 	@Test
 	public void testGoPlay() {
+		String test = "Test Level Objectif 9.0";
 		try {
 			level.init(10, 10);
 			for (int x = 0; x<level.getWidth(); x++) {
@@ -625,12 +908,15 @@ public abstract class TestLevelAbstract extends AssertionTests{
 			level.setEntrance(8, 5);
 			level.setNature(5, 6, Nature.METAL);
 			level.setExit(5, 5);
-			level.goPlay();
-			assertTrue(level.isEditing() == false);
-		}
-		catch (ContractError e) {
-			System.err.println(e.getMessage());
-			assertTrue(false);
+			try {
+				level.goPlay();
+				assertion(test, level.isEditing() == false);
+			}
+			catch (ContractError e) {
+				assertion(test+" :"+e.getMessage(),false);
+			}
+		}catch (ContractError e) {
+			assertion(test+": erreur a l'initialisation du test: "+e.getMessage(), false);
 		}
 	}
 
